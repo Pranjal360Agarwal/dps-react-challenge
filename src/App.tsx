8b3868ed542e699ce4cd94e913080977e26f1b38
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-07-23 11:51:58 +0530

    Add basic App component structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+const App: React.FC<object> = () => {
+	return (
+		<div className="container">
+			<h1>Customer Management</h1>
+		</div>
+	);
+};
+
+export default App;
