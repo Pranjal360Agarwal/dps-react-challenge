f9d77bd9cfaee61119d645984f1013b88838f2ce
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-07-23 12:58:04 +0530

    Add Filter component structure

---
+++ src/components/Filter.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
+interface FilterProps {
+  onNameFilter: (name: string) => void;
+  onCityFilter: (city: string) => void;
+  onHighlightChange: (highlight: boolean) => void;
+  customers: { address: { city: string } }[];
+}
+
+const Filter: React.FC<FilterProps> = ({ onNameFilter, onCityFilter, onHighlightChange, customers }) => {
+	const [name, setName] = useState('');
+	const [city, setCity] = useState('');
+	const [highlight, setHighlight] = useState(false);
+
+	return (
+		<div className="filter-container">
+			<input 
+				type="text" 
+				placeholder="Name" 
+				value={name} 
+				onChange={e => setName(e.target.value)} 
+				className="filter-input"
+			/>
+			<select 
+				onChange={e => {
+					setCity(e.target.value);
+					onCityFilter(e.target.value);
+				}} 
+				className="filter-select"
+			>
+				<option value="">Select city</option>
+			</select>
+			<label className="highlight-checkbox">
+				<input 
+					type="checkbox" 
+					checked={highlight} 
+					onChange={e => {
+						setHighlight(e.target.checked);
+						onHighlightChange(e.target.checked);
+					}} 
+				/> Highlight oldest per city
+			</label>
+		</div>
+	);
+};
+
+export default Filter;
