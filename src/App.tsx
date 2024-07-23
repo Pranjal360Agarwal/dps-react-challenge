a1ee119fa331f952b1d970f224100ce45b9032cd
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-07-23 11:55:22 +0530

    Add state for customers

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App: React.FC<object> = () => {
+const [customers, setCustomers] = useState<Customer[]>([]);
+
 	return (
 		<div className="container">
 			<h1>Customer Management</h1>
